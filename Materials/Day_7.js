2a6445e58e0e8efa5dfe75f6856a6df63cd396e7
Fadly Kayo <fadly.kayo@rebelworks.co>
2019-11-29 11:34:54 +0700

    Add material day 7

--- Materials/Day_7.js
+++ Materials/Day_7.js
@@ -1,44 +1,67 @@
-let arrayInputValue = [];
+function add_value() {
+  // Untuk mengambil nilai input
+  let inputValue = document.getElementById("input_value").value;
 
-function add_list() {
-  let inputValue = document.getElementById("input_add").value;
-  let inputNode = document.createTextNode(inputValue);
-
-  arrayInputValue.push(inputValue);
-
+  // Untuk membuat element Span
+  // <span></span>
   let span = document.createElement("SPAN");
-  span.className = "span_list";
-  span.appendChild(inputNode);
 
-  let br = document.createElement("BR");
-  document.getElementById("list_container").appendChild(span);
-  document.getElementById("list_container").appendChild(br);
+  // Untuk memberi element Span sebuah class
+  // <span class="content_span"></span>
+  span.className = "content_span";
 
-  let spanLength = document.getElementsByClassName("span_list").length;
+  // Untuk membuat isi text dari element Span
+  let spanText = document.createTextNode(inputValue);
+
+  // Untuk menggabungkan isi text ke dalam element Span
+  // <span class="content_span">inputValue</span>
+  span.appendChild(spanText);
+
+  // Untuk menggabungkan span ke dalam div Content
+  let content = document.getElementById("content");
+  content.appendChild(span);
+
+  document.getElementById("input_value").value = "";
+
+  // let buttonInput = document.getElementById("input_button");
+  //
+  // buttonInput.addEventListener("keypress", function(event) {
+  //   console.log(event)
+  // });
 
   // Check browser support
-  if (typeof(Storage) !== "undefined") {
-    localStorage.setItem('arrayInputValue', arrayInputValue);
-  } else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-  }
-
-  document.getElementById("input_add").value = "";
-};
-
-console.log(localStorage.length)
-
-if (localStorage.length) {
-  for (let i = 0; i < localStorage.length; i++) {
-    let span = document.createElement("SPAN");
-    span.className = "span_list";
-    span.value = localStorage.getItem('arrayInputValue')[i];
-
-    let br = document.createElement("BR");
-    document.getElementById("list_container").appendChild(span);
-    document.getElementById("list_container").appendChild(br);
-  }
+  // if (typeof(Storage) !== "undefined") {
+  //   localStorage.setItem('arrayInputValue', arrayInputValue);
+  // } else {
+  //   document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  // }
+  //
+  // document.getElementById("input_add").value = "";
 }
 
-// Retrieve
-console.log(localStorage.getItem('arrayInputValue'));
+// if (localStorage.length) {
+//   for (let i = 0; i < localStorage.length; i++) {
+//     let span = document.createElement("SPAN");
+//     span.className = "span_list";
+//     span.value = localStorage.getItem('arrayInputValue')[i];
+//
+//     let br = document.createElement("BR");
+//     document.getElementById("list_container").appendChild(span);
+//     document.getElementById("list_container").appendChild(br);
+//   }
+// }
+//
+// // Retrieve
+// console.log(localStorage.getItem('arrayInputValue'));
+
+/*
+Resources:
+https://www.w3schools.com/howto/howto_js_todolist.asp
+https://www.w3schools.com/tags/tag_input.asp
+https://www.w3schools.com/tags/tag_button.asp
+https://www.w3schools.com/js/js_htmldom.asp
+https://www.w3schools.com/js/js_htmldom_document.asp
+https://www.w3schools.com/jsref/met_document_createtextnode.asp
+https://www.w3schools.com/jsref/met_node_appendchild.asp
+https://www.w3schools.com/jsref/met_document_getelementsbyclassname.asp
+*/
